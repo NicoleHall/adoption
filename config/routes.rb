b08589ad9433d091cfe74a85fd28fdebe13de01d
Nicole Hall <nhall@pairin.com>
2017-02-06 17:34:21 -0700

    adds view for donate with placeholder copy for stripe integration

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'hall_facts#index'
-  get '/dontate', to: 'hall_facts#donate'
+  get '/donate', to: 'hall_facts#donate'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
